becc83914cc06fe58dcdcc9d1e4a85dc3d5ce2ab
Yan Kulinych <ykulinych@gmail.com>
2025-02-14 16:34:52 +0000

    bootstrap: cors & prefix & logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
   await app.listen(process.env.PORT ?? 3000);
+
+  const url = await app.getUrl();
+  Logger.debug(`Application is running on: ${url}`);
 }
+
 bootstrap();
